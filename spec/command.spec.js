78cfbdaa421d32a192f0125d6075f4505d7f799e
oriokalani <orio.kalani@gmail.com>
2023-11-16 13:06:29 -0600

    message class complete, test 1-6 passed

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -4,9 +4,20 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Command class", function() {
-
+  //! Test 1
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  
+  //! Test 2
+  it("constructor sets command type", function() {
+    let command = new Command('test');
+    expect(command.commandType).toEqual('test');
+  });
 
+  //! Test 3
+  it("constructor sets a value passes in as a the 2nd argument", function() {
+    let command = new Command('test', 'number');
+    expect(command.value).toEqual('number');
+  });
 });
